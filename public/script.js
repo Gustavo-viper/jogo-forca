3ba143ff83134720a1e3d5e034e2a4835c42ab46
Gustavo-viper <studioailvatattoo@gmail.com>
2026-07-12 15:24:46 -0300

    atualização

--- public/script.js
+++ public/script.js
@@ -3,6 +3,17 @@ const somErro = new Audio("/audio/erro.mp3");
 const somVenceu = new Audio("/audio/venceu.mp3");
 const somPerdeu = new Audio("/audio/perdeu.mp3");
 let nomeJogador = "";
+let moedas = 0;
+
+let partidas = 0;
+
+let derrotas = 0;
+
+let sequencia = 0;
+
+let melhorSequencia = 0;
+
+let tempoTotal = 0;
 
 const palavras = [
     "NODE",
@@ -23,7 +34,11 @@ let erros = 0;
 let pontos = 0;
 let tempo = 60;
 let cronometro;
-
+let nivelJogador =1;
+let xp = 0;
+let vitorias = 0;
+let palavrasAcertadas = 0;
+let conquistas = [];
 
 const desenhos = [
 
@@ -126,13 +141,53 @@ function atualizarTela(){
 <h2>🏆 VOCÊ VENCEU!</h2>
 <h3>+100 Pontos</h3>
 
-somVenceu.play();
-
 `;
+
+sequencia++;
+if (sequencia>melhorSequencia){
+melhorSequencia=sequencia;
+}
+
+atualizarEstatisticas();somVenceu.play();
 
         clearInterval(cronometro);
 
 pontos += 100;
+
+moedas +=25;
+
+document.getElementById("moedas").innerHTML = moedas;
+
+
+salvarProgresso();
+
+moedas: moedas,
+
+vitorias++;
+
+palavrasAcertadas++;
+
+if(vitorias==1)
+desbloquearConquista("Primeira Vitória");
+
+if(vitorias==10)
+desbloquearConquista("10 Vitórias");
+
+if(pontos>=1000)
+desbloquearConquista("1000 Pontos");
+
+if(nivelJogador>=5)
+desbloquearConquista("Nível 5");
+
+if(palavrasAcertadas>=50)
+desbloquearConquista("50 Palavras Acertadas");
+
+
+ganharXP(25);
+
+
+
+
 
 document.getElementById("pontos").innerHTML = pontos;
 
@@ -143,13 +198,15 @@ salvarRanking();
         document.getElementById("mensagem").innerHTML =
         document.getElementById("mensagem").innerHTML = `
 <h2>☠ GAME OVER</h2>
-<h3>A palavra era:</h3>
-<h1>${palavra}</h1>
-
-somPerdeu.play();
-
+<h3>A palavra era:</h3><h1>${palavra}</h1>
 `;
+
+derrotas++;
+sequencia=0;
+
+atualizarEstatisticas();
     }
+    somPerdeu.play();
 
 }
 
@@ -367,6 +424,10 @@ function iniciarJogo(){
 
     novoJogo();
 
+    partidas++;
+    atualizarEstatisticas();
+
+
 }
 
 function voltarMenu(){
@@ -392,8 +453,213 @@ window.onload = () => {
     }
 
     carregarRanking();
+    carregarProgresso();
 
 };
+
+function ganharXP(valor){
+
+    xp += valor;
+
+    while(xp >= 100){
+
+        xp -= 100;
+
+        nivelJogador++;
+
+        salvarProgresso();
+
+    }
+
+    document.getElementById("xp").innerHTML = xp;
+
+    document.getElementById("nivelJogador").innerHTML = nivelJogador;
+
+    document.getElementById("barraXP").style.width = xp + "%";
+
+}
+
+function desbloquearConquista(nome){
+
+    if(conquistas.includes(nome))
+        return;
+
+    conquistas.push(nome);
+
+    atualizarConquistas();
+
+    salvarProgresso();
+
+}
+
+function atualizarConquistas(){
+
+    let lista="";
+
+    conquistas.forEach(c=>{
+
+        lista += `<li>🏅 ${c}</li>`;
+
+    });
+
+    document.getElementById("conquistas").innerHTML = lista;
+
+}
+
+function salvarProgresso(){
+
+    const dados = {
+
+        nome: nomeJogador,
+
+        pontos: pontos,
+
+        xp: xp,
+
+        nivel: nivelJogador,
+
+        vitorias: vitorias,
+
+        palavrasAcertadas: palavrasAcertadas,
+
+        conquistas: conquistas
+
+    };
+
+    localStorage.setItem("forcaSave", JSON.stringify(dados));
+
+}
+
+function carregarProgresso(){
+
+    const save = JSON.parse(localStorage.getItem("forcaSave"));
+
+    if(!save) return;
+
+    nomeJogador = save.nome;
+
+    pontos = save.pontos;
+
+    xp = save.xp;
+
+    nivelJogador = save.nivel;
+
+    vitorias = save.vitorias;
+
+    palavrasAcertadas = save.palavrasAcertadas;
+
+    conquistas = save.conquistas || [];
+
+    document.getElementById("pontos").innerHTML = pontos;
+
+    document.getElementById("xp").innerHTML = xp;
+
+    document.getElementById("nivelJogador").innerHTML = nivelJogador;
+
+    document.getElementById("barraXP").style.width = xp + "%";
+
+    atualizarConquistas();
+
+    moedas = save.moedas // 0;
+
+    document.getElementById("moedas").innerHTML = moedas;
+
+
+}
+
+function comprarTemaVerde(){
+
+    if(moedas<50){
+
+        alert("Moedas insuficientes");
+
+        return;
+
+    }
+
+    moedas-=50;
+
+    document.getElementById("moedas").innerHTML=moedas;
+
+    document.body.className="temaVerde";
+
+}
+
+function comprarTemaAzul(){
+
+    if(moedas<50){
+
+        alert("Moedas insuficientes");
+
+        return;
+
+    }
+
+    moedas-=50;
+
+    document.getElementById("moedas").innerHTML=moedas;
+
+    document.body.className="temaAzul";
+
+}
+
+function comprarTemaVermelho(){
+
+    if(moedas<50){
+
+        alert("Moedas insuficientes");
+
+        return;
+
+    }
+
+    moedas-=50;
+
+    document.getElementById("moedas").innerHTML=moedas;
+
+    document.body.className="temaVermelho";
+
+}
+
+function comprarTemaDourado(){
+
+    if(moedas<100){
+
+        alert("Moedas insuficientes");
+
+        return;
+
+    }
+
+    moedas-=100;
+
+    document.getElementById("moedas").innerHTML=moedas;
+
+    document.body.className="temaDourado";
+
+}
+
+function atualizarEstatisticas(){
+
+document.getElementById("partidas").innerHTML=partidas;
+
+document.getElementById("vitoriasTela").innerHTML=vitorias;
+
+document.getElementById("derrotas").innerHTML=derrotas;
+
+document.getElementById("acertos").innerHTML=palavrasAcertadas;
+
+document.getElementById("sequencia").innerHTML=melhorSequencia;
+
+document.getElementById("nivelTela").innerHTML=nivelJogador;
+
+document.getElementById("moedasTela").innerHTML=moedas;
+
+document.getElementById("tempoTotal").innerHTML=tempoTotal;
+
+}
+
+
 
 carregarRanking();
 novoJogo();
